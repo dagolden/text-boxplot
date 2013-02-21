995119e6daff1463af1593015868a732b8294e24
David Golden <dagolden@cpan.org>
2013-02-20 22:14:01 -0500

    update example with hashref in constructor

--- examples/render.pl
+++ examples/render.pl
@@ -6,6 +6,6 @@ use FindBin qw($Bin);
 use lib "$Bin/../lib";
 use Text::BoxPlot;
 
-my $tbp = Text::BoxPlot->new( with_scale => 1 );
+my $tbp = Text::BoxPlot->new( {with_scale => 1} );
 
 say for $tbp->render( [ 'test data', -2.5, -1, 0, 1, 2.5 ] );
